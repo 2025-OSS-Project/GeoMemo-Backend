a52c7d431c3c3b3de641fabeab581befdc5d7a85
Hyoseok Jang <lapaella1200422@gmail.com>
2025-08-12 22:26:07 +0900

    fix: 메모 조회 반환값 수정
    기존의 메모리스트에 메모 내용을 포함하는 방식에서 메모 리스트에는 메모 내용을 빼는 방식으로 수정. 이로 인해 리스폰스값 변경

--- api/memo.py
+++ api/memo.py
@@ -4,7 +4,7 @@ from typing import List
 
 from core.deps import get_current_user
 from models import model
-from schemas.memo import CreateMemoRequest, LocationSchema, MemoSchema, MemoScrapSchema, UpdateMemoRequest, UserSchema
+from schemas.memo import CreateMemoRequest, LocationSchema, MemoListSchema, MemoSchema, MemoScrapSchema, UpdateMemoRequest, UserSchema
 from schemas.response import APIResponse
 from crud.memo import (
     create_memo, delete_memo, get_memo, get_memo_detail,
@@ -15,7 +15,7 @@ from db.database import get_db
 
 router = APIRouter(prefix="/api/memo", tags=["Memo"])
 
-@router.post("/", response_model=APIResponse[MemoSchema])
+@router.post("/", response_model=APIResponse[int])
 async def create_memo_api(
     memo_create: CreateMemoRequest,
     db: Session = Depends(get_db),
@@ -26,30 +26,7 @@ async def create_memo_api(
         user_id=current_user.user_id,
         memo_create=memo_create,
     )
-
-    memo_schema = MemoSchema(
-        memoId=memo.memo_id,
-        title =memo.title,
-        content=memo.content,
-        createdAt=memo.createdAt,
-        updatedAt=memo.updatedAt,
-        isPublic=memo.is_public,
-        fileUrl=[photo.photo_url for photo in memo.photos],
-        location=LocationSchema(
-            name=memo_create.location_name,
-            latitude=memo_create.location_latitude,
-            longitude=memo_create.location_longitude,
-            address=memo_create.location_address,
-            category=memo_create.location_category,
-        ),
-        user=UserSchema(
-            userId=memo.user.user_id,
-            username=memo.user.nickname,
-            photoUrl=memo.user.profile_image_url,
-        ),
-    )
-
-    return APIResponse(success=True, data=memo_schema)
+    return APIResponse(success=True, data=memo.memo_id)
 
 
 @router.post("/delete/{memo_id}", response_model=APIResponse[int])
@@ -58,7 +35,7 @@ def delete_memo_api(memo_id: int, db: Session = Depends(get_db)):
     return APIResponse(success=True, data=memo_id)
 
 
-@router.post("/update/{memo_id}", response_model = APIResponse[MemoSchema])
+@router.post("/update/{memo_id}", response_model = APIResponse[int])
 async def update_memo_api(
     memo_update: UpdateMemoRequest,
     memo_id: int,
@@ -71,33 +48,10 @@ async def update_memo_api(
         memo_id = memo_id,
         memo_update=memo_update,
     )
-
-    response_data = MemoSchema(
-        memoId=memo.memo_id,
-        title=memo.title,
-        content=memo.content,
-        createdAt=memo.createdAt,
-        updatedAt=memo.updatedAt,
-        isPublic=memo.is_public,
-        fileUrl=[photo.photo_url for photo in memo.photos],
-        location=LocationSchema(
-            name=memo.location.name,
-            latitude=memo.location.latitude,
-            longitude=memo.location.longitude,
-            address=memo.location.address,
-            category=memo.location.category,
-        ),
-        user=UserSchema(
-            userId=memo.user.user_id,
-            username=memo.user.nickname,
-            photoUrl=memo.user.profile_image_url,
-        ),
-    )
-
-    return APIResponse(success=True, data=response_data)
+    return APIResponse(success=True, data=memo.memo_id)
 
 
-@router.get("/all",response_model = APIResponse[MemoSchema])
+@router.get("/all",response_model = APIResponse[List[MemoListSchema]])
 def read_all_memos(
     view_setting: str = "all",
     lat1: float = None,  # 왼쪽 위 위도
@@ -113,11 +67,9 @@ def read_all_memos(
         memo_data = {
             "memoId": memo.memo_id,
             "title": memo.title,
-            "content": memo.content,
             "createdAt": memo.createdAt.isoformat(),
             "updatedAt": memo.updatedAt.isoformat(),
             "isPublic": memo.is_public,
-            "fileUrl": [photo.photo_url for photo in memo.photos],
             "location": {
                 "name": memo.location.name,
                 "latitude": memo.location.latitude,
@@ -132,11 +84,10 @@ def read_all_memos(
             },
         }
         data.append(memo_data)
-    print(data)
     return APIResponse(success=True, data=data)
 
 
-@router.get("/scrap", response_model=APIResponse[List[MemoSchema]])
+@router.get("/scrap", response_model=APIResponse[List[MemoListSchema]])
 def get_scrapped_memos(
     db: Session = Depends(get_db),
     current_user=Depends(get_current_user),
@@ -147,11 +98,9 @@ def get_scrapped_memos(
         memo_data = {
             "memoId": memo.memo_id,
             "title": memo.title,
-            "content": memo.content,
             "createdAt": memo.createdAt.isoformat(),
             "updatedAt": memo.updatedAt.isoformat(),
             "isPublic": memo.is_public,
-            "fileUrl": [photo.photo_url for photo in memo.photos],
             "location": {
                 "name": memo.location.name,
                 "latitude": memo.location.latitude,
@@ -200,7 +149,7 @@ def memo_info(
     return APIResponse(success=True, data=memo_schema)
 
 
-@router.get("/", response_model=APIResponse[List[MemoSchema]])
+@router.get("/", response_model=APIResponse[List[MemoListSchema]])
 def get_all_memos(
     current_user=Depends(get_current_user),
     db: Session = Depends(get_db),
@@ -211,11 +160,9 @@ def get_all_memos(
         memo_data = {
             "memoId": memo.memo_id,
             "title": memo.title,
-            "content": memo.content,
             "createdAt": memo.createdAt.isoformat(),
             "updatedAt": memo.updatedAt.isoformat(),
             "isPublic": memo.is_public,
-            "fileUrl": [photo.photo_url for photo in memo.photos],
             "location": {
                 "name": memo.location.name,
                 "latitude": memo.location.latitude,
@@ -249,14 +196,14 @@ def scrap_memo_route(
     return APIResponse(success=True, data=data)
 
 
-@router.post("/unscrap/{memo_id}")
+@router.post("/unscrap/{memo_id}", response_model=APIResponse[bool])
 def unscrap_memo_route(
     memo_id: int,
     current_user=Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    unscrap_memo(db, current_user.user_id, memo_id)
-    return APIResponse(success=True, data=None)
+    memo = unscrap_memo(db, current_user.user_id, memo_id)
+    return APIResponse(success=True, data=memo)
 
 @router.get("/check-is-scraped/{memo_id}")
 def check_is_scraped(
@@ -271,7 +218,7 @@ def check_is_scraped(
 
     return {"is_scraped": is_scraped}
 
-@router.get("/user/{user_id}", response_model=APIResponse[List[MemoSchema]])
+@router.get("/user/{user_id}", response_model=APIResponse[List[MemoListSchema]])
 def get_user_memos(
     user_id: int,
     db: Session = Depends(get_db),
@@ -285,11 +232,9 @@ def get_user_memos(
         memo_data = {
             "memoId": memo.memo_id,
             "title": memo.title,
-            "content": memo.content,
             "createdAt": memo.createdAt.isoformat(),
             "updatedAt": memo.updatedAt.isoformat(),
             "isPublic": memo.is_public,
-            "fileUrl": [photo.photo_url for photo in memo.photos],
             "location": {
                 "name": memo.location.name,
                 "latitude": memo.location.latitude,
